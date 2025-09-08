913eb1d53a3281c327948a32f79534cee5499dfc
Eugenio <geustodj@gmail.com>
2025-09-08 12:45:08 -0500

    Implement Out port
    Se construye el puerto de salida, este servirá para conectarse con la capa de infraestructura a través de un adaptador.
    Es aquí donde iniciará el concepto de inversión de dependencias

---
+++ app/Application/CarreraTaxi/Port/Out/CarreraTaxiRepositoryPort.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Application\CarreraTaxi\Port\Out;
+
+use App\Application\CarreraTaxi\Dto\Query\ListCarreraTaxiQuery;
+use App\Domain\CarreraTaxi\Entity\CarreraTaxi;
+use App\Domain\CarreraTaxi\ValueObject\CarreraTaxiId;
+
+/** Puerto de salida del módulo CarreraTaxi.
+*
+* Define lo que la aplicación NECESITA de la infraestructura (p. ej. persistencia),
+* sin acoplarse a cómo se implementa (DB, ORM, API externa).
+*
+* la infraestructura lo implementa mediante adaptadores concretos.
+*/
+
+interface CarreraTaxiRepositoryPort
+{
+  public function create(CarreraTaxi $carreraTaxi): void;
+  public function update(CarreraTaxi $carreraTaxi): void;
+  public function delete(CarreraTaxi $carreraTaxi): void;
+  public function findById(CarreraTaxiId $id): CarreraTaxi;
+  public function findAll(?ListCarreraTaxiQuery $query=null): array;
+}
