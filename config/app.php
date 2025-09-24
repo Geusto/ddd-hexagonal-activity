d675f459ea07fa3da7c51d61ce4e2e9a8e334396
Eugenio <geustodj@gmail.com>
2025-09-24 12:21:59 -0500

    provider for CarreraTaxiProvider

--- config/app.php
+++ config/app.php
@@ -181,6 +181,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        App\Infrastructure\Entrypoint\Rest\CarreraTaxi\Providers\CarreraTaxiServiceProvider::class,
 
         /*
          * Package Service Providers...
